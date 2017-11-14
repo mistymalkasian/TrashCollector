ddf8e4f3d94df81e5a28fb3eb802bf001a68c71c
Misty Malkasian <andrewandmisty@gmail.com>
2017-11-14 15:53:28 -0600

    Found and uncommented the part that was messing everything up

--- TrashCollector/TrashCollector/Models/AccountViewModels.cs
+++ TrashCollector/TrashCollector/Models/AccountViewModels.cs
@@ -49,6 +49,10 @@ namespace TrashCollector.Models
     public class LoginViewModel
     {
         [Required]
+        [Display (Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
@@ -65,9 +69,17 @@ namespace TrashCollector.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "UserRoles")]
+        public string UserRoles { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display (Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
