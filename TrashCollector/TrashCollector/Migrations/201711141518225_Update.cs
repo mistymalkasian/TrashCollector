b563c197d4dc35c91190f5776cf449f7824a189e
Misty Malkasian <andrewandmisty@gmail.com>
2017-11-14 09:26:49 -0600

    Customer and Employee models and controllers created

---
+++ TrashCollector/TrashCollector/Migrations/201711141518225_Update.cs
@@ -0,0 +1,31 @@
+namespace TrashCollector.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Address = c.String(),
+                        VacationStartDate = c.DateTime(nullable: false),
+                        VacationEndDate = c.DateTime(nullable: false),
+                        RegularPickupDay = c.String(),
+                    })
+                .PrimaryKey(t => t.CustomerID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
---
+++ TrashCollector/TrashCollector/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime VacationStartDate { get; set; }
+        public DateTime VacationEndDate { get; set; }
+        public string RegularPickupDay { get; set; }
+    }
+}
